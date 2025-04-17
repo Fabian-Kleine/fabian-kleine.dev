3c1552efdb070f19db63636ff44b45b3f252a69b
Fabian Kleine <fabian.kleine@rhiem.com>
2025-04-17 12:28:28 +0200

    feat: change amount of particles

--- components/magicui/theme-particles.tsx
+++ components/magicui/theme-particles.tsx
@@ -6,14 +6,14 @@ import { useEffect, useState } from "react";
 export default function ThemeParticles() {
     const { resolvedTheme } = useTheme();
     const [color, setColor] = useState("#ffffff");
-    const [quantity, setQuantity] = useState(20);
+    const [quantity, setQuantity] = useState(75);
 
     useEffect(() => {
         setColor(resolvedTheme === "dark" ? "#ffffff" : "#000000");
-        setQuantity(resolvedTheme === "dark" ? 40 : 150);
+        setQuantity(resolvedTheme === "dark" ? 75 : 200);
     }, [resolvedTheme]);
 
     return (
-        <Particles className="absolute inset-0" quantity={quantity} ease={80} color={color} />
+        <Particles className="absolute inset-0 z-10" quantity={quantity} ease={80} color={color} />
     );
 }
