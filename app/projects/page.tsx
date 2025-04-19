0e79b74bd829b009287fa7164b03374ee7102f5e
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2025-04-19 16:54:42 +0200

    feat: display projects on projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,35 @@
-export default function Projects() {
+import Project from "@/components/project";
+import { ProjectMetaData } from "@/types";
+
+const projectsSlugs = [
+    "weather-app",
+    "mywishlists",
+];
+
+const getProjects = async () => {
+    const featuredProjects = await Promise.all(
+        projectsSlugs.map(async (slug) => {
+            const { metadata } = await import(`@/projects/${slug}.mdx`) as { metadata: ProjectMetaData };
+            return metadata;
+        })
+    );
+    return featuredProjects;
+}
+
+export default async function Projects() {
+    const projects = await getProjects();
+
     return (
-        <h1>Projects</h1>
+        <section className="w-full border-t bg-background px-6 lg:px-36 pt-24 mt-4">
+            <h1 className="tracking-tighter text-2xl sm:text-3xl md:text-4xl lg:text-5xl/none font-bold">All My Projects</h1>
+            <div className="space-y-28 my-28">
+                {projects.map((project, index) => (
+                    <Project
+                        key={index}
+                        metadata={project}
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
