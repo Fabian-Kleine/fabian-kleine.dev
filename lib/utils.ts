48591459e4a667c3c30487154cf5c5cdda62ccad
Fabian Kleine <fabian.kleine@rhiem.com>
2026-05-04 13:40:25 +0200

    feat: add Expo to tech stack and update button cursor style

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,8 +7,21 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getSimpleIconsUrl(icon: string, theme: string) {
   let color = icon
-  if ((icon == "github" || icon == "shadcnui" || icon == "next.js" || icon == "vercel" || icon == "express") && theme === "dark") {
+  if (
+    (icon == "github" ||
+      icon == "shadcnui" ||
+      icon == "next.js" ||
+      icon == "vercel" ||
+      icon == "express" ||
+      icon == "expo"
+    )
+    && theme === "dark") {
     color = "ffffff"
   }
+
+  if (icon === "prisma") {
+    color = theme === "dark" ? "ffffff" : "2D3748"
+  }
+
   return `https://cdn.simpleicons.org/${icon}/${color}`
 }
